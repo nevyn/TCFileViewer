480ac3d25f1b21f80bebe59c6400dce779351c74
Joachim Bengtsson <joachimb@gmail.com>
2013-10-15 21:42:58 +0200

    text editor save button

--- TCFileViewer/Viewers/TCTextViewerController.m
+++ TCFileViewer/Viewers/TCTextViewerController.m
@@ -9,27 +9,44 @@
 #import "TCTextViewerController.h"
 
 @implementation TCTextViewerController {
-    NSString *_contents;
+    UITextView *_textView;
+    NSURL *_url;
 }
 - (id)initWithURL:(NSURL*)path error:(__autoreleasing NSError**)err
 {
     if(!(self = [super initWithURL:path error:err])) return nil;
-    _contents = [NSString stringWithContentsOfURL:path encoding:NSUTF8StringEncoding error:err];
-    if(!_contents)
+    NSString *contents = [NSString stringWithContentsOfURL:path encoding:NSUTF8StringEncoding error:err];
+    if(!contents)
         return nil;
+    
+    _url = path;
+    _textView = [[UITextView alloc] init];
+    _textView.text = contents;
     self.title = path.lastPathComponent;
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(save:)];
+    
     return self;
 }
+
 - (void)loadView
 {
-    UITextView *tv = [[UITextView alloc] init];
-    tv.text = _contents;
-    self.view = tv;
+    self.view = _textView;
 }
+
+- (IBAction)save:(id)sender
+{
+    NSError *err;
+    if(![_textView.text writeToURL:_url atomically:NO encoding:NSUTF8StringEncoding error:&err]) {
+        [[[UIAlertView alloc] initWithTitle:@"Couldn't save document" message:err.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+}
+
 + (BOOL)canViewDocumentAtURL:(NSURL*)url
 {
     return [@[@"txt", @"text", @"log", @"json", @"plist"] containsObject:url.pathExtension.lowercaseString];
 }
+
 + (UIImage*)thumbIcon;
 {
     return [UIImage imageNamed:@"GenericTextIcon"];
